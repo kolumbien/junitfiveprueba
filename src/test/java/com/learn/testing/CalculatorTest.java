29f704ac981013b247db9018c30440549bf5db0f
Adriana Suarez <adri@spreadshirt.net>
2018-06-21 12:27:30 +0200

    use junit-jupiter-params

--- src/test/java/com/learn/testing/CalculatorTest.java
+++ src/test/java/com/learn/testing/CalculatorTest.java
@@ -1,14 +1,37 @@
 package com.learn.testing;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.function.Supplier;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 class CalculatorTest {
-    @DisplayName("fuzz 3")
-    @Test
-    public void testNumberThree(){
 
+    /*
+    * We need to add  <artifactId>junit-jupiter-params</artifactId> dependecy for @ParameterizedTest and @CsvSource
+    * */
+
+    @ParameterizedTest(name = "{0} + {1} = {2}")
+    @CsvSource(value = {
+            "0,    1,   1",
+            "1,    2,   3",
+            "49,  51, 100",
+            "1,  100, 101"
+    })
+    public void testWithParameters(int first, int second, int expectedResult) {
+        Calculator calculator = new Calculator();
+        assertEquals(expectedResult, calculator.add(first, second),
+                () -> first + " + " + second + " should equal " + expectedResult);
+
+        assertEquals(expectedResult, calculator.add(first, second),
+                new Supplier<String>() {
+                    @Override
+                    public String get() {
+                        return first + " + " + second + " should equal " + expectedResult;
+                    }
+                });
     }
 }
